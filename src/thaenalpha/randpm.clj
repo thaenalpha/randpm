bccc6de10c92336d5f2453b28dfa20a95dd8a428
Nopanun Laochunhanun <nopanun@live.com>
2021-06-26 09:37:24 +0700

    randpm: rm gen-class, add (crypto)(archive-format)

--- src/thaenalpha/randpm.clj
+++ src/thaenalpha/randpm.clj
@@ -1,7 +1,19 @@
-(ns thaenalpha.randpm
-  (:gen-class))
+(ns thaenalpha.randpm)
 
 (defn -main
   "Random windows package managers"
   []
   (println (rand-nth ["winget" "choco" "scoop"])))
+
+(defn crypto
+  "Random Crypto currencies"
+  [opts]
+  (println (rand-nth ["BTC" "ETH" "LINK" "BNB" "TRX" "DOT" "EOS" "LTC" "BCH" "XRP" "ETC" "FIL" "ADA"
+           "burger" "ont" "cake" "btt" "zil" "xlm" "jfin" "crv" "doge" "sand" "rune" "unfi" "OMG"
+           "fet" "xvs" "alpha" "fio" "icp" "Theta" "waves"])))
+
+(defn archive-format
+  "Random Oh-My-Zsh Universal archive plugin format"
+  [opts]
+  (println (rand-nth ["7z" "bz2" "gz" "lzma" "lzo" "rar" "tar" "tbz" "tgz" "tlz"
+                      "txz" "tZ" "xz" "Z" "zip" "zst"])))
